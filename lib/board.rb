7d9dcd5e6be4cdf89372dece30d54a8b78b58dc4
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-20 15:22:31 +0200

    ran Rubocop on the current status

--- lib/board.rb
+++ lib/board.rb
@@ -1,29 +1,33 @@
+# frozen_string_literal: true
+
+# A Board is initialized as empty.
+# It has a to_s method for pretty printing, and it can process a move made by a player
 class Board
   attr_accessor :state
-  
+
   def initialize
-    self.state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  
+    self.state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
   def process_move(move, player_symbol)
     # Game board is 1-9 for human convenience
-    # When zero-indexed, we can utilize integer division and modulo to 
-    # get the row and column values of a given move. 
+    # When zero-indexed, we can utilize integer division and modulo to
+    # get the row and column values of a given move.
     row = (move - 1) / 3
     col = (move - 1) % 3
-    self.state[row][col] = player_symbol
+    state[row][col] = player_symbol
   end
 
   def to_s
     print '-' * 15
     puts
-    for row in self.state
-        for cell in row
-            print "| #{cell} |"
-        end
-        puts
-        print '-' * 15
-        puts
+    state.each do |row|
+      row.each do |cell|
+        print "| #{cell} |"
+      end
+      puts
+      print '-' * 15
+      puts
     end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
+# A Player has a symbol (X or O) and can make a move
 class Player
   def initialize(symbol)
     @symbol = symbol
   end
 
   def make_move(options)
-    while move = gets.chomp.to_i
-        if options.include?(move)
-            return move
-        else
-            puts "Invalid move!"
-        end
+    while (move = gets.chomp.to_i)
+      return move if options.include?(move)
+
+      puts 'Invalid move!'
+
     end
   end
 end
