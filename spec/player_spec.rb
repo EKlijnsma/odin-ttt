0c806fcbc711e491f157d5624511247792074774
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-22 10:16:15 +0200

    complete tests for player spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/player'
 
 describe Player do
   describe '#make_move' do
+    # Set up the subject and an options array for use in all cases
     subject(:player) { described_class.new('X') }
     let(:options) { [1, 2, 3, 4, 5] }
     
@@ -11,22 +12,32 @@ describe Player do
       before do
         allow(player).to receive(:gets).and_return('2')
       end
-
+      
       it 'returns the move' do
         move = player.make_move(options)
         expect(move).to eq(2)
       end
     end
-
+    
     context 'when an invalid move is made once' do
-      xit 'returns a message once and then returns the valid move' do
-      
+      before do
+        allow(player).to receive(:gets).and_return('9','2')
+      end
+      it 'returns a message once and then returns the valid move' do
+        expect(player).to receive(:puts).once
+        move = player.make_move(options)
+        expect(move).to eq(2)
       end
     end
     
     context 'when an invalid move is made twice' do
-      xit 'returns a message twice and then returns the valid move' do
-      
+      before do
+        allow(player).to receive(:gets).and_return('9', '8','2')
+      end
+      it 'returns a message twice and then returns the valid move' do
+        expect(player).to receive(:puts).twice
+        move = player.make_move(options)
+        expect(move).to eq(2)      
       end
     end
   end
