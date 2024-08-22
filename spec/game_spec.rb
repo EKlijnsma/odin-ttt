8beed024fcafd5a7f0448f078b5ad319b9222c96
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-22 11:41:49 +0200

    define tests for game spec

---
+++ spec/game_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game'
+
+describe Game do
+  describe '#options' do
+    it 'returns all 9 options when the board is empty' do
+    end
+    it 'returns limited options when the board is partially full' do
+    end
+    it 'returns an empty array when the board is full' do
+    end
+  end
+
+  describe '#switch_players' do
+    it 'switches from player X to player O' do
+    end
+    it 'switches from player O to player X' do
+    end
+  end
+end
