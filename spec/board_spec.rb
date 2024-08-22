1b2861b9bfbe9320eb4a63c4cfd1f40828e33ea8
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-22 11:15:27 +0200

    write tests for board.winner?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,24 +16,40 @@ describe Board do
     end
     
     context 'when the board is partially occupied' do
-      xit 'correctly returns no win' do
+      it 'correctly returns no win' do
+        board.state = [[1, 'X', 3], ['X', 'O', 6], [7, 'O', 9]]
+        expect(board).not_to be_winner
       end
-      xit 'detects a vertical win' do
+      it 'detects a vertical win' do
+        board.state = [['X', 'O', 3], ['X', 'O', 6], [7, 'O', 'X']]
+        expect(board).to be_winner
       end
-      xit 'detects a horizontal win' do
+      it 'detects a horizontal win' do
+        board.state = [[1, 'X', 3], ['X', 'O', 'X'], ['O', 'O', 'O']]
+        expect(board).to be_winner
       end
-      xit 'detects a diagonal win' do
+      it 'detects a diagonal win' do
+        board.state = [['X', 2, 3], ['O', 'X', 6], [7, 'O', 'X']]
+        expect(board).to be_winner
       end
     end
     
     context 'when the board is full' do
-      xit 'correctly returns no win' do
+      it 'correctly returns no win' do
+        board.state = [['O', 'X', 'O'], ['X', 'X', 'O'], ['X', 'O', 'X']]
+        expect(board).not_to be_winner
       end
-      xit 'detects a vertical win' do
+      it 'detects a vertical win' do
+        board.state = [['O', 'X', 'O'], ['X', 'X', 'O'], ['O', 'X', 'X']]
+        expect(board).to be_winner
       end
-      xit 'detects a horizontal win' do
+      it 'detects a horizontal win' do
+        board.state = [['O', 'X', 'O'], ['X', 'X', 'X'], ['X', 'O', 'O']]
+        expect(board).to be_winner
       end
-      xit 'detects a diagonal win' do
+      it 'detects a diagonal win' do
+        board.state = [['X', 'X', 'O'], ['O', 'X', 'O'], ['X', 'O', 'X']]
+        expect(board).to be_winner
       end
     end
 
